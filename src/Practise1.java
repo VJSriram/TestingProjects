652c55b6fe279a2658eb2305dfe5e38e8aa2c907
VJSriram <vuijay@outlook.com>
2017-03-10 00:54:33 -0600

    StringHandling

---
+++ src/Practise1.java
@@ -0,0 +1,20 @@
+
+public class Practise1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Sting Methods
+		
+		// Payment $100 paid
+		//CharAt(Index) Prular
+		String str="Payment $100 paid";
+		System.out.println(str.charAt(8));
+		
+		//IndexOf Singular
+		String strl="Payments $100 paid";
+		System.out.println(str.indexOf("$"));
+		System.out.println(str.substring(0));
+	}
+
+}
